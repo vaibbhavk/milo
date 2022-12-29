73cdf2520050a43a1b7cf5b274cf13a1735e9d9a
Vaibhav Kesharwani <vaibhav.vk2128@gmail.com>
2022-12-29 11:15:50 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Milo
+
+## Create and Track milestones for any task.
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+      <p>This is an example of using speech synthesis in English.</p>
+    </div>
+  );
+}
+
+export default App;
